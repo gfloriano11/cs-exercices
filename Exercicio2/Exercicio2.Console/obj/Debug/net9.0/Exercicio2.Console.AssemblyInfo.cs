19694b875a6cb29c80c089dc7243ce69cf32c7dc
gfloriano11 <gflorianodev@gmail.com>
2025-08-15 19:24:31 -0300

    chore: update run files

--- Exercicio2/Exercicio2.Console/obj/Debug/net9.0/Exercicio2.Console.AssemblyInfo.cs
+++ Exercicio2/Exercicio2.Console/obj/Debug/net9.0/Exercicio2.Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio2.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c34989cd069fa117e4b91be7f3fd122dad84c9c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+088310ccc91337a2ea575f2645d1df35328a6259")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio2.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio2.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
